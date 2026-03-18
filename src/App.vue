f292a14a95faece8963287e2c060b7d861c952e1
Victorpragasam <DavidRajasekar.Victorpragasam@Emerson.com>
2026-03-18 15:56:13 +0530

    first commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <h1>Welcome to Vite + Vue</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#app {
+  text-align: center;
+  margin-top: 60px;
+}
+</style>
