59f3602948384e16506f530ed8bf7a78541787a8
Davidrase1984 <davidrase@gmail.com>
2026-04-01 15:59:17 +0530

    Use explicit SQL config instead of connection string for reliable auth

--- api/src/functions/db.js
+++ api/src/functions/db.js
@@ -1,16 +1,30 @@
 const sql = require('mssql')
 
-const connectionString = process.env.AZURE_SQL_CONNECTION_STRING
-
 let pool = null
 
 async function getConnection() {
-  if (!connectionString) {
-    throw new Error('AZURE_SQL_CONNECTION_STRING environment variable is not set')
-  }
   if (pool) return pool
 
-  pool = await sql.connect(connectionString)
+  const server = process.env.AZURE_SQL_SERVER || 'azuresqlfcec.database.windows.net'
+  const database = process.env.AZURE_SQL_DATABASE || 'Recuritmentdb'
+  const user = process.env.AZURE_SQL_USER || 'fcec'
+  const password = process.env.AZURE_SQL_PASSWORD || ''
+
+  if (!password) {
+    throw new Error('AZURE_SQL_PASSWORD environment variable is not set')
+  }
+
+  pool = await sql.connect({
+    server: server,
+    database: database,
+    user: user,
+    password: password,
+    options: {
+      encrypt: true,
+      trustServerCertificate: false
+    }
+  })
+
   pool.on('error', () => { pool = null })
   return pool
 }
